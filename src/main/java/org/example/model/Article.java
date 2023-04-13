cc852bb58466c279807b2a171859a1813bed560f
AliBoulal <aliboulal93@gmail.com>
2023-04-13 18:33:05 +0000

    Create Article Class and add fields, constructors, getters & setters ,toString

---
+++ src/main/java/org/example/model/Article.java
@@ -0,0 +1,62 @@
+package org.example.model;
+
+public class Article {
+    protected int code;
+    protected String Designation;
+    protected float prix;
+    protected Categorie categorie;
+
+    /** Constructor **/
+    /**
+     * Constructeur par défaut
+     */
+    public Article() {
+    }
+
+    /**
+     * Constructeur d'initialisation
+     * @param code
+     * @param designation
+     * @param prix
+     * @param categorie choix entre Informatique et Bureautique
+     */
+    public Article(int code, String designation, float prix, Categorie categorie) {
+        this.code = code;
+        Designation = designation;
+        this.prix = prix;
+        this.categorie = categorie;
+    }
+
+    /** Getters & Setters **/
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    /**
+     * Prix de l'article
+     * @return
+     */
+    public float getPrix() {
+        return prix;
+    }
+
+    /**
+     * Changer le prix de l'article
+     * @param prix
+     */
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    @Override
+    public String toString() {
+        return "Article:" + code +
+                "; Designation='" + Designation + '\'' +
+                "; prix=" + prix +
+                "; categorie=" + categorie ;
+    }
+}
